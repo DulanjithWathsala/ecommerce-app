2974db648314b4fc80acfed0a18d8940049f543e
M.D.W.De Silva <wdulanjith@gmail.com>
2025-02-06 19:44:30 +0530

    Modal added to view selected product

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { fetchAllProducts } from "../services/api.js";
+import Modal from "./Modal.jsx";
 
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [isFetched, setIsFetched] = useState(false);
   const [error, setError] = useState(null);
+
+  const [open, setOpen] = useState(false);
+
+  const [selectedProduct, setSelectedProduct] = useState(null);
 
   useEffect(() => {
     async function getProducts() {
@@ -24,6 +29,10 @@ export default function Products() {
     getProducts();
   }, []);
 
+  function onCloseModal() {
+    setOpen(false);
+  }
+
   return (
     <div className="max-w-7xl mx-auto p-6">
       <h2 className="text-2xl font-bold text-gray-900 mb-6">Our Products</h2>
@@ -40,6 +49,10 @@ export default function Products() {
         <p className="text-gray-600">No products found!</p>
       )}
 
+      {open && (
+        <Modal open product={selectedProduct} onCloseModal={onCloseModal} />
+      )}
+
       {!error && isFetched && products.length > 0 && (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {products.map((product) => (
@@ -50,6 +63,10 @@ export default function Products() {
               description={product.description}
               price={product.price}
               category={product.category}
+              onViewProduct={() => {
+                setSelectedProduct(product);
+                setOpen(true);
+              }}
             />
           ))}
         </div>
